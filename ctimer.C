22baf5da5a8c66f566fa04bf1922ce51db14e089
atropine <brendan@packetfire.org>
2012-01-27 07:40:28 -0800

    the start of something beautiful

---
+++ ctimer.C
@@ -0,0 +1,10 @@
+#include <Python.h>
+#include <time.h>
+#include <string.h>
+unsigned long flag;
+
+int main(){
+	
+
+	flag = 1;
+	 
---
+++ wrappers.py
@@ -0,0 +1,20 @@
+# FICS requires all information be sent in this format:
+# <12> rnbqkb-r pppppppp -----n-- -------- ----P--- -------- PPPPKPPP RNBQ-BNRB -1 0 0 1 1 0 7 Newton Einstein 1 2 12 39 39 119 122 2 K/e1-e2 (0:06) Ke2 0 
+
+import os
+import sys
+import multiprocess
+import time
+
+
+
+
+positions = [pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8]  # each position is a string of dashes and upper or lowercase characters
+
+class constrng()  #i don't think  need to initialize all of these, but i forgot how to code
+	def __init__(self, number, positions, color, push, ws, wl, bs, bl, drawnum, gamenum, wname, bname, relation, stime, ctime, wpt, bpt, srt, brt, move, coords, turn_time, notes, flip):
+		number = '<'x'>'				
+		
+		 
+	def timer:
+		
