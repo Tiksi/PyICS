b15a0843f24b15ae82bda8f9a93ca00701b28137
atropine <brendan@packetfire.org>
2012-01-27 17:54:06 -0800

    some timer code

--- ctimer.C
+++ ctimer.C
@@ -1,10 +1,49 @@
+//=========================================================================
+// Name        : timer
+// Author      : wintermute
+// Version     : alpha
+// Copyright is gay.
+// Description : a timer function
+//=========================================================================
 #include <Python.h>
 #include <time.h>
 #include <string.h>
-unsigned long flag;
+unsigned double clock_t, flag_on, flag_off;
+double elapsed;
 
-int main(){
+static PyObject *
+ctimer_time(PyObject *self, PyObject *args)
+
+{
+    const char *command;
+    int sts;
+
+    if (!PyArg_ParseTuple(args, "flg", &command))
+        return NULL;
+    sts = system(command);
+    return Py_BuildValue("clk", sts);
+}
+
+
+
+int elapsed
+{
+	
+	if (flag_match_start)
+	{	
+	flag_on = clock();
+	if (flag_match_end)
+	{
+		flag_off = clock();
+		elapsed = ((double) (flag_off-flag_on)) / CLOCKS_PER_SEC;
+	}
+	}
+	return elapsed;
+}
+void turn_time()
+{
+	if
 	
 
-	flag = 1;
-	 
+
+
